e125e85eeb158a7f0bf89cf30495f1389178dc58
Chase Lambert <chaselambert@gmail.com>
2024-01-19 14:31:16 -0500

    first commit - polylith tutorial

---
+++ components/user/src/se/example/user/core.clj
@@ -0,0 +1,5 @@
+(ns se.example.user.core)
+
+
+(defn hello [name]
+  (str "Hello " name "!"))
---
+++ components/user/src/se/example/user/interface.clj
@@ -0,0 +1,7 @@
+(ns se.example.user.interface
+  (:require
+    [se.example.user.core :as core]))
+
+
+(defn hello [name]
+  (core/hello name))
